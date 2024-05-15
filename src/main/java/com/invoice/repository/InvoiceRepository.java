822b6e6da931bb95b22ea82feb06861d9525ade2
dharmamarri <dharmamarri1729@gmail.com>
2024-05-15 16:26:37 +0530

    crud repo

--- src/main/java/com/invoice/repository/InvoiceRepository.java
+++ src/main/java/com/invoice/repository/InvoiceRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import com.invoice.entity.Invoice;
 
-public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+public interface InvoiceRepository extends CrudRepository<Invoice, Integer> {
 
 	@Query(value = "select * from invoice where user_id=?",
 			nativeQuery = true)
--- src/main/java/com/invoice/repository/UserRepository.java
+++ src/main/java/com/invoice/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.invoice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.invoice.entity.User;
 
-public interface UserRepository  extends JpaRepository<User, Integer>{
+public interface UserRepository  extends CrudRepository<User, Integer>{
 
 	public User findByUserNameAndPassword(String userName, String password);
 	
